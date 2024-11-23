b883e1af2ca21210dadba9c770e3d4c661eb548f
rinaadem <ajuka1414@gmail.com>
2024-11-23 19:38:28 +0300

    Зарегистрировал модель article в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'created_at']
+    list_display_links = ['id', 'title']
+    list_filter = ['author']
+    search_fields = ['title', 'content']
+    fields = ['title', 'author', 'content', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
