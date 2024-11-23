cdfb6420f00b274b64819baa11d77bc03143f5c1
rinaadem <ajuka1414@gmail.com>
2024-11-23 21:25:37 +0300

    Переписал создание статьи, использовал модель Article

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -15,11 +15,8 @@ def articles_view(request):
     if request.method == 'GET':
         return render(request, 'article_create.html')
     elif request.method == 'POST':
-        article = {
-            'title': request.POST.get('title'),
-            'content': request.POST.get('content'),
-            'author': request.POST.get('author'),
-        }
-        # ArticleDB.articles.append(article)
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.POST.get('author')
+        Article.objects.create(title=title, content=content, author=author)
         return HttpResponseRedirect('/')
-        # return render(request, 'article.html', context={'article': article})
